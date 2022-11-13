246625b108b47f52288300c0bf9c971c705f6aee
Rhowen01 <rraju002@ucr.edu>
2022-11-12 20:55:17 -0800

    adjust template

--- components/Header/index.js
+++ components/Header/index.js
@@ -174,7 +174,7 @@ const Header = ({ handleWorkScroll, handleAboutScroll, isBlog }) => {
               </Button>
             )}
 
-            <Button onClick={() => window.open("mailto:hello@chetanverma.com")}>
+            <Button onClick={() => window.open("mailto:rraju002@ucr.edu")}>
               Contact
             </Button>
 
